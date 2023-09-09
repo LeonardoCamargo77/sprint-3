d83bc1e6ea459355c28d72a23e80e4dff96a3959
LeonardoCamargo77 <leonardocamargx@gmail.com>
2023-09-09 02:34:15 -0300

    Estilização de páginas + Menu e Rodapé.

--- src/components/Rodape.jsx
+++ src/components/Rodape.jsx
@@ -2,7 +2,7 @@ export default function Rodape(){
 
     return(
         <footer>
-            <p>Rodapé</p>
+            <p> &copy; TODOS OS DIREITOS RESERVADOS - Leonardo Lucena 552537 - Nathan Nunes 552539 - Ana Paula 552513  - Calina Thalya 552523  - Gabriel Henrique 99463</p>
         </footer>
     )
 
